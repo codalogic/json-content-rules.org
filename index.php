600c5009a7fb720111719d6d7824a844b35ebc17
Pete Cordell <pete.git@codalogic.com>
2015-10-22 17:21:51 +0100

    Initial setup (redirect to Github for now)

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php header("Location: http://codalogic.github.io/jcr/",TRUE,307); ?>
+<html><body>
+We're still hard at work building this. Meanwhile, take a look at the version on Github:
+
+<a href='http://codalogic.github.io/jcr/'>http://codalogic.github.io/jcr/</a>
+</body></html>
