9f219989765ef928f814c1c1f45571605f37e37a
Pete Cordell <pete.git@codalogic.com>
2018-03-13 14:39:27 +0000

    Added additonal JCR examples

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -40,7 +40,7 @@ function main_content()
 				is not JSON but is "JSON-like", possessing the conciseness and
 				utility that has made JSON popular.</p>
 
-				<p>As an example, the following JSON taken from RFC 7159:</p>
+				<p>As an example, the following JSON is taken from RFC 7159:</p>
 
 <pre>
 {
@@ -58,7 +58,7 @@ function main_content()
     }
 }
 </pre>
-				<p>can be described using JCR as:</p>
+				<p>It can be described using JCR as:</p>
 <pre>
 {
     "Image" : {
@@ -102,7 +102,7 @@ $height = "Height" : 0..1024
 		<p>
 			As <code>$width</code> and <code>$height</code> are often used together, the above can
 			be further simplified by putting them in a group and using the group as a mixin. Below,
-			this is done in the group named <code>$dimensions</code>.
+			this is done in the group named <code>$dimensions</code>:
 		</p>
 <pre>
 {
@@ -144,6 +144,94 @@ $dimensions = ( $width, $height )
 $width = "Width" : 0..1280
 $height = "Height" : 0..1024
 $id = integer
+</pre>
+		<p>
+			Or, perhaps the entire <code>"Thumbnail"</code> member could be
+			extracted into a separate rule:
+		</p>
+<pre>
+{
+    "Image" : {
+        $dimensions,
+        "Title" : string,
+        $thumbnail,
+        "Animated" : boolean,
+        "IDs" : [ $id * ]
+    }
+}
+
+$thumbnail =
+    "Thumbnail" : {
+        "Url" : uri,
+        $dimensions
+    }
+
+$dimensions = ( $width, $height )
+$width = "Width" : 0..1280
+$height = "Height" : 0..1024
+$id = integer
+</pre>
+		<p>
+			If the <code>"Thumbnail"</code> member was optional, this can be
+			indicated using the Kleene <code>?</code> operator:
+		</p>
+<pre>
+{
+    "Image" : {
+        $dimensions,
+        "Title" : string,
+        $thumbnail ?,
+        "Animated" : boolean,
+        "IDs" : [ $id * ]
+    }
+}
+
+$thumbnail =
+    "Thumbnail" : {
+        "Url" : uri,
+        $dimensions
+    }
+
+$dimensions = ( $width, $height )
+$width = "Width" : 0..1280
+$height = "Height" : 0..1024
+$id = integer
+</pre>
+		<p>
+			JCR allows modular message specification using <code>#import</code> directives.  As screen and image dimensions
+			might be a common, cross-protocol feature, they can be separated into a separate module.
+			Such a module would be incorporated into the example as:
+		</p>
+<pre>
+#import org.ietf.geometry as geometry
+
+{
+    "Image" : {
+        $geometry.dimensions,
+        "Title" : string,
+        $thumbnail ?,
+        "Animated" : boolean,
+        "IDs" : [ $id * ]
+    }
+}
+
+$thumbnail =
+    "Thumbnail" : {
+        "Url" : uri,
+        $geometry.dimensions
+    }
+
+$id = integer
+</pre>
+		<p>
+			Where <code>org.ietf.geometry</code> might be defined as:
+		</p>
+<pre>
+#ruleset-id org.ietf.geometry
+
+$dimensions = ( $width, $height )
+$width = "Width" : 0..1280
+$height = "Height" : 0..1024
 </pre>
 
 				<h3>
